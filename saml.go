46b4404c426080d038c2c538c72c9bd9b1848457
Tom Miller <tmiller@eab.com>
2016-09-30 09:28:00 -0400

    Create parser for SAML

--- saml.go
+++ saml.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
 
-func Hi() {
-	fmt.Println("Hi")
+type Document struct {
+	XMLName xml.Name    `xml:"Response"`
+	Attrs   []Attribute `xml:"Assertion>AttributeStatement>Attribute"`
+}
+
+type Attribute struct {
+	Name   string   `xml:"Name,attr"`
+	Values []string `xml:"AttributeValue"`
+}
+
+func main() {
+	xmlFile, err := ioutil.ReadFile("sample.xml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var q Document
+	xml.Unmarshal(xmlFile, &q)
+
+	for _, attr := range q.Attrs {
+		for _, val := range attr.Values {
+			fmt.Println(val)
+		}
+	}
 }
