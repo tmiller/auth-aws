2c47d6ab1b9edc503d7a4513c8e7671b897a7505
Tom Miller <tmiller@eab.com>
2016-10-04 00:26:34 -0400

    Add failover to CLI if missing any values

--- adfs_config.go
+++ adfs_config.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/howeyc/gopass"
 	"gopkg.in/ini.v1"
 )
 
@@ -24,14 +27,31 @@ func newADFSConfig() *ADFSConfig {
 		checkError(err)
 	}
 
+	reader := bufio.NewReader(os.Stdin)
+
 	if val, ok := os.LookupEnv("ADFS_USER"); ok {
 		adfsConfig.Username = val
+	} else if adfsConfig.Username == "" {
+		fmt.Printf("Username: ")
+		user, err := reader.ReadString('\n')
+		checkError(err)
+		adfsConfig.Username = strings.Trim(user, "\n")
 	}
 	if val, ok := os.LookupEnv("ADFS_PASS"); ok {
 		adfsConfig.Password = val
+	} else if adfsConfig.Password == "" {
+		fmt.Printf("Password: ")
+		pass, err := gopass.GetPasswd()
+		checkError(err)
+		adfsConfig.Password = string(pass[:])
 	}
 	if val, ok := os.LookupEnv("ADFS_HOST"); ok {
 		adfsConfig.Hostname = val
+	} else if adfsConfig.Hostname == "" {
+		fmt.Printf("Hostname: ")
+		host, err := reader.ReadString('\n')
+		checkError(err)
+		adfsConfig.Hostname = strings.Trim(host, "\n")
 	}
 
 	return adfsConfig
