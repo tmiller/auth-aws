d27374974528506ec8d651a0aadf9c1c7fe3372d
Tom Miller <tmiller@eab.com>
2016-10-11 21:44:13 -0500

    Remove abandoned tests
    We will not finish out writing these test because of how complicated
    capturing stdin and stdout multiple ways will be. Likely this will be
    refactored to a better solution in the future.

--- adfs_test.go
+++ adfs_test.go
@@ -17,45 +17,6 @@ func compareADFSConfg(t *testing.T, expected *AdfsClient, actual *AdfsClient) {
 	}
 }
 
-//func captureStdinStdout(t *testing.T, input string, f func()) string {
-//	oldStdout := os.Stdout
-//	oldStdin := os.Stdin
-//
-//	rIn, wIn, err := os.Pipe()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	os.Stdin = rIn
-//
-//	rOut, wOut, err := os.Pipe()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	os.Stdout = wOut
-//
-//	wIn.WriteString(input)
-//
-//	f()
-//
-//	outC := make(chan string)
-//
-//	go func() {
-//		var buf bytes.Buffer
-//		io.Copy(&buf, rOut)
-//		outC <- buf.String()
-//	}()
-//
-//	os.Stdout = oldStdout
-//	wOut.Close()
-//
-//	out := <-outC
-//
-//	os.Stdin = oldStdin
-//	wIn.Close()
-//
-//	return out
-//}
-
 func TestLoadSettingsFile(t *testing.T) {
 	expected := &AdfsClient{"foo", "bar", "adfs.test"}
 
@@ -86,21 +47,3 @@ func TestLoadEnvVars(t *testing.T) {
 
 	compareADFSConfg(t, expected, actual)
 }
-
-// func TestLoadAskVars(t *testing.T) {
-//
-// 	expOut := "Username: Password: Hostname: "
-// 	expected := &AdfsClient{"foo", "bar", "adfs.test"}
-//
-// 	actual := new(AdfsClient)
-//
-// 	input := "foo\nbar\nadfs.test\n"
-// 	actOut := captureStdinStdout(t, input, func() {
-// 		loadAskVars(actual)
-// 	})
-//
-// 	if expOut != actOut {
-// 		t.Errorf("incorrect output\nexpected: %v\nactual: %v", expOut, actOut)
-// 	}
-// 	compareADFSConfg(t, expected, actual)
-// }
